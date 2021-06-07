cd30ba12921e16310d61f3ce49b946347b1e7ccc
Grace Mugoiri <gracemugoiri@gmail.com>
2021-06-07 13:29:48 +0300

    setup web3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Change this
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(web3.version)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Change this
+          </a>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
